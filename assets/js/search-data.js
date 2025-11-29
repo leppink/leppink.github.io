639d33f7b3fc925009d33fdef543e18b5660412f
leppink <evleppink@gmail.com>
2025-11-29 04:41:57 +0000

    Deploying to gh-pages from @ leppink/leppink.github.io@37d3f94f620e6874584081d2a44824825cfd0f53 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/assets/pdf/Leppink_CV.pdf";
+            window.location.href = "/assets/pdf/Leppink_CV_Nov2025.pdf";
           },
         },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
       
